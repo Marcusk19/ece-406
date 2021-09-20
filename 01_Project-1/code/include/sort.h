4e3ecaa2b82df75fe53e30eaa149ef445de01959
Abdullah Mughrabi <atmughra@ncsu.edu>
2021-09-20 19:11:37 -0400

    [MAKEFILE] updated makefile rules

--- 01_Project-1/code/include/sort.h
+++ 01_Project-1/code/include/sort.h
@@ -3,14 +3,23 @@
 
 #include "edgelist.h"
 #include "graph.h"
-// Order edges by id of a source vertex, 
+// Order edges by id of a source vertex,
 // using the Counting Sort
 // Complexity: O(E + V)
 
-// struct Graph* radixSortEdgesBySourceOpenMP (struct Graph* graph);
-// struct Graph* radixSortEdgesBySourceMPI (struct Graph* graph);
-// struct Graph* radixSortEdgesBySourceHybrid (struct Graph* graph);
-struct Graph* countSortEdgesBySource (struct Graph* graph);
+#ifdef OPENMP_HARNESS
+struct Graph *radixSortEdgesBySourceOpenMP (struct Graph *graph);
+#endif
+
+#ifdef MPI_HARNESS
+struct Graph *radixSortEdgesBySourceMPI (struct Graph *graph);
+#endif
+
+#ifdef HYBRID_HARNESS
+struct Graph *radixSortEdgesBySourceHybrid (struct Graph *graph);
+#endif
+
+struct Graph *countSortEdgesBySource (struct Graph *graph);
 
 extern int numThreads;
 
--- 01_Project-1/code/src/sort.c
+++ 01_Project-1/code/src/sort.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+
+#ifdef OPENMP_HARNESS
 #include <omp.h>
-// #include <mpi.h>
+#endif
+
+#ifdef MPI_HARNESS
+#include <mpi.h>
+#endif
+
+#ifdef HYBRID_HARNESS
+#include <omp.h>
+#include <mpi.h>
+#endif
 
 #include "sort.h"
 #include "graph.h"
 
-struct Graph* countSortEdgesBySource (struct Graph* graph){
+struct Graph *countSortEdgesBySource (struct Graph *graph)
+{
 
     int i;
     int key;
@@ -15,25 +27,29 @@ struct Graph* countSortEdgesBySource (struct Graph* graph){
     struct Edge *sorted_edges_array = newEdgeArray(graph->num_edges);
 
     // auxiliary arrays, allocated at the start up of the program
-    int *vertex_count = (int*)malloc(graph->num_vertices*sizeof(int)); // needed for Counting Sort
+    int *vertex_count = (int *)malloc(graph->num_vertices * sizeof(int)); // needed for Counting Sort
 
-    for(i = 0; i < graph->num_vertices; ++i) {
+    for(i = 0; i < graph->num_vertices; ++i)
+    {
         vertex_count[i] = 0;
     }
 
     // count occurrence of key: id of a source vertex
-    for(i = 0; i < graph->num_edges; ++i) {
+    for(i = 0; i < graph->num_edges; ++i)
+    {
         key = graph->sorted_edges_array[i].src;
         vertex_count[key]++;
     }
 
     // transform to cumulative sum
-    for(i = 1; i < graph->num_vertices; ++i) {
+    for(i = 1; i < graph->num_vertices; ++i)
+    {
         vertex_count[i] += vertex_count[i - 1];
     }
 
     // fill-in the sorted array of edges
-    for(i = graph->num_edges - 1; i >= 0; --i) {
+    for(i = graph->num_edges - 1; i >= 0; --i)
+    {
         key = graph->sorted_edges_array[i].src;
         pos = vertex_count[key] - 1;
         sorted_edges_array[pos] = graph->sorted_edges_array[i];
@@ -51,18 +67,29 @@ struct Graph* countSortEdgesBySource (struct Graph* graph){
 
 }
 
-// struct Graph* radixSortEdgesBySourceOpenMP (struct Graph* graph){
+#ifdef OPENMP_HARNESS
+struct Graph *radixSortEdgesBySourceOpenMP (struct Graph *graph)
+{
 
-//         // printf("*** START Radix Sort Edges By Source OpenMP *** \n");
+    printf("*** START Radix Sort Edges By Source OpenMP *** \n");
+    return graph;
+}
+#endif
 
-// }
-// struct Graph* radixSortEdgesBySourceMPI (struct Graph* graph){
+#ifdef MPI_HARNESS
+struct Graph *radixSortEdgesBySourceMPI (struct Graph *graph)
+{
 
-//         // printf("*** START Radix Sort Edges By Source MPI*** \n");
+    printf("*** START Radix Sort Edges By Source MPI*** \n");
+    return graph;
+}
+#endif
 
-// }
-// struct Graph* radixSortEdgesBySourceHybrid (struct Graph* graph){
+#ifdef HYBRID_HARNESS
+struct Graph *radixSortEdgesBySourceHybrid (struct Graph *graph)
+{
 
-//         // printf("*** START Radix Sort Edges By Source Hybrid*** \n");
-
-// }
+    printf("*** START Radix Sort Edges By Source Hybrid*** \n");
+    return graph;
+}
+#endif
