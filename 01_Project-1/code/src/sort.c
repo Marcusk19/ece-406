2b95c2b1c658fc36bec32359cec4124c454ebe5e
Marcusk19 <mskok@ncsu.edu>
2021-09-22 21:03:32 -0400

    new type of parallization

--- 01_Project-1/code/src/sort.c
+++ 01_Project-1/code/src/sort.c
@@ -8,19 +8,94 @@
 #include "sort.h"
 #include "graph.h"
 
-
 struct Graph* countSortEdgesBySource (struct Graph* graph){
     //printf("--Parallized Count Sort-- \n");
-
-    int printThreads = 1;
-    int i = 0;
+    int printThreads = 0;
+    //int i = 0;
     int key;
     int pos;
+    int P;
     struct Edge *sorted_edges_array = newEdgeArray(graph->num_edges);
 
     // auxiliary arrays, allocated at the start up of the program
-    int *vertex_count = (int*)malloc(graph->num_vertices*sizeof(int)); // needed for Counting Sort
+    //int *vertex_count = (int*)malloc(graph->num_vertices*sizeof(int)); // needed for Counting Sort
+    
+    // from piazza
+    printf("num_edges = %d\n", graph->num_edges);
 
+    printf("parallel region start \n");
+    #pragma omp parallel
+    {
+        // start parallel region
+        int i = 0;
+        int tid = omp_get_thread_num();
+        printf("tid = %d\n", tid);
+
+
+            P = omp_get_num_threads();
+            //printf("P = %d\n", P);
+            int *vertex_count = (int*)malloc(P*(graph->num_vertices * sizeof(int)));
+        
+
+        #pragma omp barrier
+        int offset_start = tid * (graph->num_edges / P);
+        int offset_end;
+        if(tid == (P-1)){
+            offset_end = offset_start + (graph->num_edges/P) + (graph->num_edges%P);
+        }
+        else{
+            offset_end = offset_start + (graph->num_edges/P);
+        }
+
+        printf("offset_start = %d and offset_end = %d\n", offset_start, offset_end);
+        // this loop will excecute in parallel but need to split i = offset_start, i < offset_end
+        // according to tid
+        #pragma omp barrier
+
+        printf("Start initialization\n");
+        for(int i = offset_start; i < offset_end; i++){
+            vertex_count[i] = 0; 
+            if(printThreads)printf("Initialize array iteration=%d thread=%d\n", i, (int)pthread_self());
+        }
+        #pragma omp barrier
+
+        printf("\nStart count\n");
+        for(int i = offset_start; i < offset_end; i++){
+            key = graph->sorted_edges_array[i].src;
+            vertex_count[key]++;
+            if(printThreads)printf("Count occurence iteration=%d thread=%d\n",i,(int)pthread_self());
+        }
+        #pragma omp barrier
+
+        printf("Start cumulative sum\n");
+        for(i = offset_start; i < graph->num_vertices; i++){
+            vertex_count[i] += vertex_count[i - 1];
+            if(printThreads)printf("Cumulative sum iteration=%d thread=%d\n",i,(int)pthread_self());
+        }
+        #pragma omp barrier
+
+        printf("Start array fill-in\n");
+        for(i = offset_start; i < offset_end; i++){
+            key = graph->sorted_edges_array[i].src;
+            pos = vertex_count[key] - 1;
+            sorted_edges_array[pos] = graph->sorted_edges_array[i];
+            vertex_count[key]--;
+            if(printThreads)printf("Fill in sorted array iteration=%d thread=%d\n",i,(int)pthread_self());
+        }
+        free(vertex_count);
+
+    } // end parallel region
+
+ 
+    printf("Freed vertex_count\n");
+    free(graph->sorted_edges_array);
+
+    graph->sorted_edges_array = sorted_edges_array;
+    //printEdgeArray(sorted_edges_array, graph->num_edges);
+    return graph;
+    
+    
+    /*
     #pragma omp parallel for shared(vertex_count,i)
     for(i=0; i < graph->num_vertices; ++i) {
         vertex_count[i] = 0;
@@ -32,7 +107,7 @@ struct Graph* countSortEdgesBySource (struct Graph* graph){
     #pragma omp parallel for shared(vertex_count,key,sorted_edges_array,i)
     for(i = 0; i < graph->num_edges; ++i) {
         key = graph->sorted_edges_array[i].src;
-        if(printThreads)printf("Count occurence iteration=%d thread=%d\n",i,(int)pthread_self());
+        //if(printThreads)printf("Count occurence iteration=%d thread=%d\n",i,(int)pthread_self());
         vertex_count[key]++;
         if(printThreads)printf("Count occurence iteration=%d thread=%d\n",i,(int)pthread_self());
     }
@@ -61,17 +136,18 @@ struct Graph* countSortEdgesBySource (struct Graph* graph){
     free(graph->sorted_edges_array);
 
     graph->sorted_edges_array = sorted_edges_array;
-    
-    return graph;
+    printEdgeArray(sorted_edges_array, graph->num_edges);
+    return graph; 
+    */
 
 }
 
-    struct Graph* radixSortEdgesBySourceOpenMP (struct Graph* graph){
+struct Graph* radixSortEdgesBySourceOpenMP (struct Graph* graph){
 
-        printf("*** START Radix Sort Edges By Source OpenMP *** \n");
-        
-        return NULL;
-    } 
+    printf("*** START Radix Sort Edges By Source OpenMP *** \n");
+    
+    return NULL;
+} 
 // struct Graph* radixSortEdgesBySourceMPI (struct Graph* graph){
 
 //         // printf("*** START Radix Sort Edges By Source MPI*** \n");
