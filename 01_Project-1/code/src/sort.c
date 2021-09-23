c1c6c24533c1e1811dcc7e18a01d8c3b9e92b338
Marcusk19 <mskok@ncsu.edu>
2021-09-23 10:00:36 -0400

    working countsort

--- 01_Project-1/code/src/sort.c
+++ 01_Project-1/code/src/sort.c
@@ -20,61 +20,89 @@
 
 struct Graph *countSortEdgesBySource (struct Graph *graph)
 {
+
+
     int printThreads = 0;
-    int i;
-    int key;
-    int pos;
-    int P;
     struct Edge *sorted_edges_array = newEdgeArray(graph->num_edges);
-
-    // auxiliary arrays, allocated at the start up of the program
-    int *vertex_count = (int *)malloc(graph->num_vertices * sizeof(int)); // needed for Counting Sort
-
-    for(i = 0; i < graph->num_vertices; ++i)
+    #pragma omp parallel
     {
-        vertex_count[i] = 0;
-        if(printThreads)printf("Initialize array iteration=%d thread=%d\n",i,(int)pthread_self());
-    }
-    #pragma omp barrier
+        int i;
+        int key;
+        int pos;
+        int P;
+        int tid = omp_get_thread_num();
+        int offset_start, offset_end;
 
-    // count occurrence of key: id of a source vertex
-    for(i = 0; i < graph->num_edges; ++i)
-    {
-        key = graph->sorted_edges_array[i].src;
-        //if(printThreads)printf("Count occurence iteration=%d thread=%d\n",i,(int)pthread_self());
-        vertex_count[key]++;
-        if(printThreads)printf("Count occurence iteration=%d thread=%d\n",i,(int)pthread_self());
-    }
-    #pragma omp barrier
+        P = omp_get_num_threads();
+        int *vertex_count = (int*)malloc(P*graph->num_edges*sizeof(int));
+        printf("malloc:%d\n", graph->num_edges*sizeof(int));
+        
+        #pragma omp barrier
 
-    #pragma omp parallel for shared(vertex_count,i)
-    // transform to cumulative sum
-    for(i = 1; i < graph->num_vertices; ++i)
-    {
-        vertex_count[i] += vertex_count[i - 1];
-        if(printThreads)printf("Cumulative sum iteration=%d thread=%d\n",i,(int)pthread_self());
-    }
-    #pragma omp barrier
+        offset_start = tid * (graph->num_edges/P);
 
-    #pragma omp parallel for shared(vertex_count,key,pos,sorted_edges_array,i)
-    // fill-in the sorted array of edges
-    for(i = graph->num_edges - 1; i >= 0; --i)
-    {
-        key = graph->sorted_edges_array[i].src;
-        pos = vertex_count[key] - 1;
-        sorted_edges_array[pos] = graph->sorted_edges_array[i];
-        vertex_count[key]--;
-        if(printThreads)printf("Fill in sorted array iteration=%d thread=%d\n",i,(int)pthread_self());
-    }
-    #pragma omp barrier
+        if(tid == (P-1)){
+            offset_end = offset_start + (graph->num_edges/P) + (graph->num_edges%P);
+        }else{
+            offset_end = offset_start + (graph->num_edges/P);
+        }
+        printf("num_edges:%d\n", graph->num_edges);
+        printf("%d to %d\n", offset_start, offset_end);
+        #pragma omp barrier
 
-    free(vertex_count);
+        // auxiliary arrays, allocated at the start up of the program
+        //int *vertex_count = (int *)malloc(graph->num_vertices * sizeof(int)); // needed for Counting Sort
+
+        printf("Started array initialization\n");
+        for(i = offset_start; i < offset_end; ++i)
+        {
+            //printf("%d\n", i);
+            vertex_count[i] = 0;
+            if(printThreads)printf("Initialize array iteration=%d thread=%d\n",i,(int)pthread_self());
+        }
+        printf("Finished array initialization\n");
+        #pragma omp barrier
+
+        // count occurrence of key: id of a source vertex
+        printf("Started count occurence\n");
+        for(i = offset_start; i < offset_end; ++i)
+        {
+            key = graph->sorted_edges_array[i].src;
+            //if(printThreads)printf("Count occurence iteration=%d thread=%d\n",i,(int)pthread_self());
+            vertex_count[key]++;
+            if(printThreads)printf("Count occurence iteration=%d thread=%d\n",i,(int)pthread_self());
+        }
+        printf("Finished count occurence\n");
+        #pragma omp barrier
+
+        // transform to cumulative sum
+        printf("Started cumulative sum\n");
+        for(i = 1; i < graph->num_vertices; ++i)
+        {
+            vertex_count[i] += vertex_count[i - 1];
+            if(printThreads)printf("Cumulative sum iteration=%d thread=%d\n",i,(int)pthread_self());
+        }
+        printf("Finished cumulative sum\n");
+        #pragma omp barrier
+
+        // fill-in the sorted array of edges
+        printf("Started fill-in\n");
+        for(i = offset_end - 1; i >= offset_start; --i)
+        {
+            key = graph->sorted_edges_array[i].src;
+            pos = vertex_count[key] - 1;
+            sorted_edges_array[pos] = graph->sorted_edges_array[i];
+            vertex_count[key]--;
+            if(printThreads)printf("Fill in sorted array iteration=%d thread=%d\n",i,(int)pthread_self());
+        }
+        printf("Finished fill-in\n");
+        #pragma omp barrier
+        free(vertex_count);
+    }  
     free(graph->sorted_edges_array);
-
     graph->sorted_edges_array = sorted_edges_array;
     printEdgeArray(sorted_edges_array, graph->num_edges);
     return graph; 
-    */
 
 }
 
