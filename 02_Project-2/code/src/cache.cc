eaffb3ad3f6fd4124924ce6b0c2db6b9017949be
Marcusk19 <mskok@ncsu.edu>
2021-11-02 15:50:10 -0400

    [UPDATE] removed unnecesary comments

--- 02_Project-2/code/src/cache.cc
+++ 02_Project-2/code/src/cache.cc
@@ -51,29 +51,7 @@ void Cache::Access(ulong addr,uchar op, int protocol)
 {
    currentCycle++;/*per cache global counter to maintain LRU order 
          among cache ways, updated on every cache access*/
-   
-   // if(op == 'w') writes++;
-   // else          reads++;
-   
-   // cacheLine * line = findLine(addr);
-   // if(line == NULL)/*miss*/
-   // {
-   //    if(op == 'w'){
-   //       writeMisses++;
-   //       busRdX++;
-   //    }
-   //    else readMisses++;
 
-   //    cacheLine *newline = fillLine(addr);
-   //       if(op == 'w') newline->setFlags(DIRTY);    
-      
-   // }
-   // else
-   // {
-   //    /**since it's a hit, update LRU and update dirty flag**/
-   //    updateLRU(line);
-   //    if(op == 'w') line->setFlags(DIRTY);
-   // }
    cacheLine *line;
    switch(protocol){
       case MSI:
